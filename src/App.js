0bfe2a63adef32ab4521d8a914a4806e8912061d
Krunal2206 <kru.dipnil@gmail.com>
2022-03-03 16:01:00 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ const App = () => {
       <div className="center_div">
         <h1>ToDo List</h1>
         <div className="add_iteam">
-          <input type="text" placeholder="Add a Items" onKeyPress={(e) => { if (e.key === "Enter") { addItems } }} value={inputList} onChange={(e) => { setInputList(e.target.value) }} />
+          <input type="text" placeholder="Add a Items" onKeyPress={(e) => { e.key === "Enter" ? addItems : "Please enter valid key" }} value={inputList} onChange={(e) => { setInputList(e.target.value) }} />
           <AddCircleOutlineOutlinedIcon className='add' onClick={addItems} />
         </div>
 
