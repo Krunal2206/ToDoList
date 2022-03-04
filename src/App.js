b2915eb474b8b38d8a7bd058144002240d80faed
Krunal2206 <kru.dipnil@gmail.com>
2022-03-04 14:40:21 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -32,24 +32,40 @@ const App = () => {
     }
   }
 
-  return (
-    <div className="main_div">
-      <div className="center_div">
-        <h1>ToDo List</h1>
-        <div className="add_iteam">
-          <input type="text" placeholder="Add a Items" onKeyPress={(e) => { e.key === "Enter" ? addItems : "Please enter valid key" }} value={inputList} onChange={(e) => { setInputList(e.target.value) }} />
-          <AddCircleOutlineOutlinedIcon className='add' onClick={addItems} />
-        </div>
+  const handleItems = (e) => {
+    if (e.key === "Enter") {
+      console.log("enter");
+      if (inputList.length === 0) {
+        window.alert("Please Add Item.");
+      } else {
+        addDoc(collection(db, 'list'), {
+          item: inputList
+        });
+        setInputList("")
+      }
+    }
+  }
 
-        <ol>
-          {
-            list && list.map((items) => {
-              return (
-                <ToDoLists key={items.id} itemList={items.doc.item} id={items.id} />
-              )
-            })
-          }
-        </ol>
+  return (
+    <div>
+      <div className='main_div'>
+        <div className="center_div">
+          <h1>ToDo List</h1>
+          <div className="add_iteam">
+            <input type="text" placeholder="Add a Items" onKeyPress={handleItems} value={inputList} onChange={(e) => { setInputList(e.target.value) }} />
+            <AddCircleOutlineOutlinedIcon className='add' onClick={addItems} />
+          </div>
+
+          <ol>
+            {
+              list && list.map((items) => {
+                return (
+                  <ToDoLists key={items.id} itemList={items.doc.item} id={items.id} />
+                )
+              })
+            }
+          </ol>
+        </div>
       </div>
     </div>
   )
